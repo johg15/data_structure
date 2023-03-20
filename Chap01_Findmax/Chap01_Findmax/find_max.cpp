011074efb6b29afaefee6cecbe6c14044fefece0
johg15 <hygijo@gmail.com>
2023-03-20 12:37:29 +0900

    chap01 project added

---
+++ Chap01_Findmax/Chap01_Findmax/find_max.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int findArrayMax(int score[], int n)
+{
+	int tmp = score[0];
+	for (int i = 1; i < n; i++) {
+		if (score[i] > tmp) {
+			tmp = score[i];
+		}
+	}
+	return tmp;
+}
+
+void main()
+{
+	int score[5] = { 12, 123, 12, 54 , 1};
+	int out;
+	out = findArrayMax(score, 5);
+	printf("result = %d", out);
+}
